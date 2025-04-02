feed5edee5ba8d19d43e8d410b6c3f6d8364b714
Risan <borhansiddque19@gmail.com>
2025-04-03 02:16:24 +0600

    Enhance styling of components with CSS; add borders and padding to cards, and update heading style

--- module-38/my-first-react-app/src/App.jsx
+++ module-38/my-first-react-app/src/App.jsx
@@ -1,41 +1,55 @@
 import './App.css'
 
 function App() {
+  const headingStyle = {
+    color: "purple",
+    backgroundColor: "white",
+    padding: "1rem",
+    borderRadius: "10px"
+  }
 
   return (
     <>
-      <h1>React App</h1>
+      <h1 style={headingStyle}>React App</h1>
       <Person></Person>
       <FavFood/>
       <Sleep></Sleep>
       <Friends name={"Rohan"} age={20} hasVehicle={"Yes"}></Friends>
+      <Friends name={"Mankir Poya"} age={22} hasVehicle={"No"}></Friends>
       <Girlfriend name={"Adiba"} phone={"01822334455"} trueLove={"Yes"}></Girlfriend>
     </>
   )
 }
 
 function Person() {
-  return <div>
+  return <div className="card">
       <h4>Hello I am from Person Component</h4>
     </div>
   
 }
 
 function FavFood() {
-  return <>
+  return <div className="card">
     <h4>My Fav Food Is : Pineapple</h4>
-  </>
+  </div>
 }
 
 function Sleep() {
+  const sleepStyle = {
+    border: "2px solid tomato",
+    padding: "20px",
+    borderRadius: "10px"
+  };
+
   return (
-    <h4>Ami Daily 7-8 Hr Ghumate pochondho kori</h4>
+    <h4 style={sleepStyle}>Ami Daily 7-8 Hr Ghumate pochondho kori</h4>
   )
 }
 
 function Friends(props) {
   return (
-    <div>
+    <div className="card">
+      <h3>Friend</h3>
       <h5>Name: {props.name}</h5>
       <p>Age: {props.age}</p>
       <p>hasVehicle: {props.hasVehicle}</p>
@@ -45,7 +59,8 @@ function Friends(props) {
 
 function Girlfriend({name, phone, trueLove}) {
   return (
-    <div>
+    <div className="card">
+      <h3>Girlfriend</h3>
       <h5>Name: {name}</h5>
       <p>Phone: {phone}</p>
       <p>True Love: {trueLove}</p>
