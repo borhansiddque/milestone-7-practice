47910e01e429e8886e2afee17f663b7572e18b4b
Risan <borhansiddque19@gmail.com>
2025-04-03 01:33:48 +0600

    Add new components for Person, FavFood, and Sleep to App

--- module-38/my-first-react-app/src/App.jsx
+++ module-38/my-first-react-app/src/App.jsx
@@ -5,7 +5,29 @@ function App() {
   return (
     <>
       <h1>React App</h1>
+      <Person></Person>
+      <FavFood/>
+      <Sleep></Sleep>
     </>
+  )
+}
+
+function Person() {
+  return <div>
+      <h4>Hello I am from Person Component</h4>
+    </div>
+  
+}
+
+function FavFood() {
+  return <>
+    <h4>My Fav Food Is : Pineapple</h4>
+  </>
+}
+
+function Sleep() {
+  return (
+    <h4>Ami Daily 7-8 Hr Ghumate pochondho kori</h4>
   )
 }
 
