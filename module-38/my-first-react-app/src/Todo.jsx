c545b0d71ed2347b96d4069b8e20834382e512b3
Risan <borhansiddque19@gmail.com>
2025-04-03 21:04:32 +0600

    Refactor Todo component for improved conditional rendering and code clarity

--- module-38/my-first-react-app/src/Todo.jsx
+++ module-38/my-first-react-app/src/Todo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 // export default function Todo({task, isDone}) {
 
@@ -14,11 +14,39 @@ import React from 'react'
 //   if (isDone === true) {
 //     return <li>Done: {task}</li>
 //   }
-  
+
 //   return <li>Pending: {task}</li>
-  
+
 // }
 
-export default function Todo({task, isDone, time=0}) {
- return isDone ? <li>Done: {task} - {time}</li> : <li>Pending: {task}</li>
+// Conditional Rendering : 3 Ternary
+// export default function Todo({ task, isDone, time = 0 }) {
+//   return isDone ? (
+//     <li>
+//       Done: {task} - {time}
+//     </li>
+//   ) : (
+//     <li>Pending: {task}</li>
+//   );
+// }
+
+// // Conditional Rendering : 4 &&
+// export default function Todo({ task, isDone, time = 0 }) {
+//   return isDone && <li>Done Tasks: {task} Time: {time}</li>
+// }
+
+// // Conditional Rendering : 4 ||
+// export default function Todo({ task, isDone, time = 0 }) {
+//   return isDone || <li>Not Done Tasks: {task} Time: {time}</li>
+// }
+
+// Conditional Rendering : 6 Use variable
+export default function Todo({task, isDone}) {
+  let listItem ;
+  if (isDone === true) {
+    listItem = <li>Done: {task}</li>
+  } else {
+    listItem = <li>Pending: {task}</li>
+  }
+  return listItem;
 }
